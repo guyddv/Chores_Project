4c1fc4108b5ce518b7b3515930606cb023237dad
Guy Daughtrey <guyddv@users.noreply.github.com>
2021-10-20 09:55:46 -0500

    Test before class separation

--- Chores_Project/DataAccess.cs
+++ Chores_Project/DataAccess.cs
@@ -11,146 +11,126 @@ namespace Chores_Project
 {
     public class DataAccess
     {
-        private string connectionString;
+        //private string connectionString;
 
         public DataAccess(string connectionString)
         {
-            Console.WriteLine("Connect to SQL Server and demo Create, Read, Update and Delete operations.");
+            Console.WriteLine("Connect to SQL Server and Create, Read, Update and Delete operations.");
 
             SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
-            builder.DataSource = "DESKTOP-5E220RG\\SQLSERVER2019";   // update me
-            builder.UserID = "sa";              // update me
-            builder.Password = "Skoal55gddv";      // update me
+            builder.DataSource = "DESKTOP-5E220RG\\SQLSERVER2019";   
+            builder.UserID = "sa";              
+            builder.Password = "Skoal55gddv";      
             builder.InitialCatalog = "Chores_Project";
-
-            // Connect to SQL
-            
-            using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
-            {
-                connection.Open();
-                Console.WriteLine("Done.");
-
-                // Create a sample database
-                Console.Write("Dropping and creating database 'SampleDB' ... ");
-                String sql = "DROP DATABASE IF EXISTS [SampleDB]; CREATE DATABASE [SampleDB]";
-                using (SqlCommand command = new SqlCommand(sql, connection))
-                {
-                    command.ExecuteNonQuery();
-                    Console.WriteLine("Done.");
-                }
-
-                Console.WriteLine("All done. Press any key to finish...");
-                Console.ReadKey(true);
-            }
         }
 
-        public void CreateDatabase()
-        {
-            Console.Write("Connecting to SQL Server ... ");
-            using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
-            {
-                connection.Open();
-                Console.WriteLine("Done.");
+        //public void CreateDatabase()
+        //{
+        //    Console.Write("Connecting to SQL Server ... ");
+        //    using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+        //    {
+        //        connection.Open();
+        //        Console.WriteLine("Done.");
 
-                // Create a sample database
-                Console.Write("Dropping and creating database 'ChoresDB' ... ");
-                String sql = "DROP DATABASE IF EXISTS [ChoresDB]; CREATE DATABASE [ChoresDB]";
-                using (SqlCommand command = new SqlCommand(sql, connection))
-                {
-                    command.ExecuteNonQuery();
-                    Console.WriteLine("Done.");
-                }
-            }
-        }
+        //        // Create a sample database
+        //        Console.Write("Dropping and creating database 'ChoresDB' ... ");
+        //        String sql = "DROP DATABASE IF EXISTS [ChoresDB]; CREATE DATABASE [ChoresDB]";
+        //        using (SqlCommand command = new SqlCommand(sql, connection))
+        //        {
+        //            command.ExecuteNonQuery();
+        //            Console.WriteLine("Done.");
+        //        }
+        //    }
+        //}
 
-        public void CreateChoreTable()
-        {
-            Console.Write("Creating sample table with data, press any key to continue...");
-            Console.ReadKey(true);
-            StringBuilder sb = new StringBuilder();
-            sb.Append("USE ChoresDB; ");
-            sb.Append("CREATE TABLE Chores ( ");
-            sb.Append(" ChoresId INT IDENTITY(1,1) NOT NULL PRIMARY KEY, ");
-            sb.Append(" ChoreName NVARCHAR(50), ");
-            sb.Append(" ChoreAssignment NVARCHAR(50) ");
-            sb.Append("); ");
-            sb.Append("INSERT INTO Chores (ChoreName, ChoreAssignment) VALUES ");
-            sb.Append("(N'Clean Kitchen', N'Guy'), ");
-            sb.Append("(N'Trash', N'Stephen'), ");
-            sb.Append("(N'Walk  Cats', N'Guy'); ");
-            sql = sb.ToString();
-            using (SqlCommand command = new SqlCommand(sql, connection))
-            {
-                command.ExecuteNonQuery();
-                Console.WriteLine("Done.");
-            }
-        }
+        //public void CreateChoreTable()
+        //{
+        //    Console.Write("Creating sample table with data, press any key to continue...");
+        //    Console.ReadKey(true);
+        //    StringBuilder sb = new StringBuilder();
+        //    sb.Append("USE ChoresDB; ");
+        //    sb.Append("CREATE TABLE Chores ( ");
+        //    sb.Append(" ChoresId INT IDENTITY(1,1) NOT NULL PRIMARY KEY, ");
+        //    sb.Append(" ChoreName NVARCHAR(50), ");
+        //    sb.Append(" ChoreAssignment NVARCHAR(50) ");
+        //    sb.Append("); ");
+        //    sb.Append("INSERT INTO Chores (ChoreName, ChoreAssignment) VALUES ");
+        //    sb.Append("(N'Clean Kitchen', N'Guy'), ");
+        //    sb.Append("(N'Trash', N'Stephen'), ");
+        //    sb.Append("(N'Walk  Cats', N'Guy'); ");
+        //    sql = sb.ToString();
+        //    using (SqlCommand command = new SqlCommand(sql, connection))
+        //    {
+        //        command.ExecuteNonQuery();
+        //        Console.WriteLine("Done.");
+        //    }
+        //}
 
-        public void AddChore()
-        {
-            Console.Write("Inserting a new row into table, press any key to continue...");
-            Console.ReadKey(true);
-            sb.Clear();
-            sb.Append("INSERT Chores (ChoreName, ChoreAssignment) ");
-            sb.Append("VALUES (@name, @assignment);");
-            sql = sb.ToString();
-            using (SqlCommand command = new SqlCommand(sql, connection))
-            {
-                command.Parameters.AddWithValue("@name", "Clean Bathroom");
-                command.Parameters.AddWithValue("@assignment", "Stephen");
-                int rowsAffected = command.ExecuteNonQuery();
-                Console.WriteLine(rowsAffected + " row(s) inserted");
-            }
-        }
+        //public void AddChore()
+        //{
+        //    Console.Write("Inserting a new row into table, press any key to continue...");
+        //    Console.ReadKey(true);
+        //    sb.Clear();
+        //    sb.Append("INSERT Chores (ChoreName, ChoreAssignment) ");
+        //    sb.Append("VALUES (@name, @assignment);");
+        //    sql = sb.ToString();
+        //    using (SqlCommand command = new SqlCommand(sql, connection))
+        //    {
+        //        command.Parameters.AddWithValue("@name", "Clean Bathroom");
+        //        command.Parameters.AddWithValue("@assignment", "Stephen");
+        //        int rowsAffected = command.ExecuteNonQuery();
+        //        Console.WriteLine(rowsAffected + " row(s) inserted");
+        //    }
+        //}
 
-        public void UpdateChore()
-        {
-            String userToUpdate = "Stephen";
-            Console.Write("Updating 'ChoreAssignment' for user '" + userToUpdate + "', press any key to continue...");
-            Console.ReadKey(true);
-            sb.Clear();
-            sb.Append("UPDATE Chores SET ChoreAssignment = N'United States' WHERE Name = @name");
-            sql = sb.ToString();
-            using (SqlCommand command = new SqlCommand(sql, connection))
-            {
-                command.Parameters.AddWithValue("@name", userToUpdate);
-                int rowsAffected = command.ExecuteNonQuery();
-                Console.WriteLine(rowsAffected + " row(s) updated");
-            }
-        }
+        //public void UpdateChore()
+        //{
+        //    String userToUpdate = "Stephen";
+        //    Console.Write("Updating 'ChoreAssignment' for user '" + userToUpdate + "', press any key to continue...");
+        //    Console.ReadKey(true);
+        //    sb.Clear();
+        //    sb.Append("UPDATE Chores SET ChoreAssignment = N'Guy' WHERE Name = @name");
+        //    sql = sb.ToString();
+        //    using (SqlCommand command = new SqlCommand(sql, connection))
+        //    {
+        //        command.Parameters.AddWithValue("@name", userToUpdate);
+        //        int rowsAffected = command.ExecuteNonQuery();
+        //        Console.WriteLine(rowsAffected + " row(s) updated");
+        //    }
+        //}
 
-        public int DeleteChore()
-        {
-            String userToDelete = "Clean Kitchen";
-            Console.Write("Deleting user '" + userToDelete + "', press any key to continue...");
-            Console.ReadKey(true);
-            sb.Clear();
-            sb.Append("DELETE FROM Chores WHERE ChoreName = @name;");
-            sql = sb.ToString();
-            using (SqlCommand command = new SqlCommand(sql, connection))
-            {
-                command.Parameters.AddWithValue("@name", userToDelete);
-                int rowsAffected = command.ExecuteNonQuery();
-                Console.WriteLine(rowsAffected + " row(s) deleted");
-            }
-        }
+        //public void DeleteChore()
+        //{
+        //    String userToDelete = "Clean Kitchen";
+        //    Console.Write("Deleting user '" + userToDelete + "', press any key to continue...");
+        //    Console.ReadKey(true);
+        //    sb.Clear();
+        //    sb.Append("DELETE FROM Chores WHERE ChoreName = @name;");
+        //    sql = sb.ToString();
+        //    using (SqlCommand command = new SqlCommand(sql, connection))
+        //    {
+        //        command.Parameters.AddWithValue("@name", userToDelete);
+        //        int rowsAffected = command.ExecuteNonQuery();
+        //        Console.WriteLine(rowsAffected + " row(s) deleted");
+        //    }
+        //}
 
-        public List<string> GetChores()
-        {
-            Console.WriteLine("Reading data from table, press any key to continue...");
-            Console.ReadKey(true);
-            sql = "SELECT ChoreId, ChoreName, ChoreAssignment FROM Chores;";
-            using (SqlCommand command = new SqlCommand(sql, connection))
-            {
+        //public void GetChores()
+        //{
+        //    Console.WriteLine("Reading data from table, press any key to continue...");
+        //    Console.ReadKey(true);
+        //    sql = "SELECT ChoreId, ChoreName, ChoreAssignment FROM Chores;";
+        //    using (SqlCommand command = new SqlCommand(sql, connection))
+        //    {
 
-                using (SqlDataReader reader = command.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        Console.WriteLine("{0} {1} {2}", reader.GetInt32(0), reader.GetString(1), reader.GetString(2));
-                    }
-                }
-            }
-        }
+        //        using (SqlDataReader reader = command.ExecuteReader())
+        //        {
+        //            while (reader.Read())
+        //            {
+        //                Console.WriteLine("{0} {1} {2}", reader.GetInt32(0), reader.GetString(1), reader.GetString(2));
+        //            }
+        //        }
+        //    }
+        //}
     }
 }
--- Chores_Project/Program.cs
+++ Chores_Project/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using System.Data.SqlClient;
 
 namespace Chores_Project
 {
@@ -6,7 +8,102 @@ namespace Chores_Project
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Connect to SQL Server and Create, Read, Update and Delete operations.");
+
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = "DESKTOP-5E220RG\\SQLSERVER2019";
+            builder.UserID = "sa";
+            builder.Password = "Skoal40gddv";
+            builder.InitialCatalog = "Chores_Project";
+
+            using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+            {
+                connection.Open();
+                Console.WriteLine("Done.");
+
+                Console.Write("Dropping and creating database 'SampleDB' ... ");
+                String sql = "DROP DATABASE IF EXISTS [ChoresDB]; CREATE DATABASE [ChoresDB]";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    command.ExecuteNonQuery();
+                    Console.WriteLine("Done.");
+                }
+
+                Console.Write("Creating sample table with data, press any key to continue...");
+                Console.ReadKey(true);
+                StringBuilder sb = new StringBuilder();
+                sb.Append("USE ChoresDB; ");
+                sb.Append("CREATE TABLE Chores ( ");
+                sb.Append(" ChoresId INT IDENTITY(1,1) NOT NULL PRIMARY KEY, ");
+                sb.Append(" ChoreName NVARCHAR(50), ");
+                sb.Append(" ChoreAssignment NVARCHAR(50) ");
+                sb.Append("); ");
+                sb.Append("INSERT INTO Chores (ChoreName, ChoreAssignment) VALUES ");
+                sb.Append("(N'Clean Kitchen', N'Guy'), ");
+                sb.Append("(N'Trash', N'Stephen'), ");
+                sb.Append("(N'Walk  Cats', N'Guy'); ");
+                sql = sb.ToString();
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    command.ExecuteNonQuery();
+                    Console.WriteLine("Done.");
+                }
+
+                Console.Write("Inserting a new row into table, press any key to continue...");
+                Console.ReadKey(true);
+                sb.Clear();
+                sb.Append("INSERT Chores (ChoreName, ChoreAssignment) ");
+                sb.Append("VALUES (@name, @assignment);");
+                sql = sb.ToString();
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@name", "Clean Bathroom");
+                    command.Parameters.AddWithValue("@assignment", "Stephen");
+                    int rowsAffected = command.ExecuteNonQuery();
+                    Console.WriteLine(rowsAffected + " row(s) inserted");
+                }
+
+                String userToUpdate = "Stephen";
+                Console.Write("Updating 'ChoreAssignment' for user '" + userToUpdate + "', press any key to continue...");
+                Console.ReadKey(true);
+                sb.Clear();
+                sb.Append("UPDATE Chores SET ChoreAssignment = N'Guy' WHERE Name = @name");
+                sql = sb.ToString();
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@name", userToUpdate);
+                    int rowsAffected = command.ExecuteNonQuery();
+                    Console.WriteLine(rowsAffected + " row(s) updated");
+                }
+
+                String userToDelete = "Clean Kitchen";
+                Console.Write("Deleting user '" + userToDelete + "', press any key to continue...");
+                Console.ReadKey(true);
+                sb.Clear();
+                sb.Append("DELETE FROM Chores WHERE ChoreName = @name;");
+                sql = sb.ToString();
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@name", userToDelete);
+                    int rowsAffected = command.ExecuteNonQuery();
+                    Console.WriteLine(rowsAffected + " row(s) deleted");
+                }
+
+                Console.WriteLine("Reading data from table, press any key to continue...");
+                Console.ReadKey(true);
+                sql = "SELECT ChoreId, ChoreName, ChoreAssignment FROM Chores;";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine("{0} {1} {2}", reader.GetInt32(0), reader.GetString(1), reader.GetString(2));
+                        }
+                    }
+                }
+            }
         }
     }
 }
